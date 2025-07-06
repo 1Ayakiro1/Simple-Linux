55ffd5c1902bd13cd4dbdf553026e6d721ccf25d
1Ayakiro1 <135359123+1Ayakiro1@users.noreply.github.com>
2025-07-06 12:23:25 +0200

    ver 0.2.0
    translation of text is work!!!

--- hotkeys_chapters/gnomehotk.py
+++ hotkeys_chapters/gnomehotk.py
@@ -1,10 +1,25 @@
 import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
-from gi.repository import Gtk, Adw
-from translations.main_titles import current_language
+from gi.repository import Gtk, Adw, Gdk
+from translations.main_titles import translations, current_language
 from code.dynamic_refs import dynamic_labels
 from translations.hotkeys_chapters import hotkeys_translations
+import os
+
+# --- CSS  ---
+css_provider = Gtk.CssProvider()
+css_path = os.path.join(os.path.dirname(__file__), "../styles/gnome_hotk.css")
+try:
+    css_provider.load_from_path(css_path)
+    Gtk.StyleContext.add_provider_for_display(
+        Gdk.Display.get_default(),
+        css_provider,
+        Gtk.STYLE_PROVIDER_PRIORITY_APPLICATION
+    )
+except Exception as e:
+    print(f"[CSS ERROR] {e}")
+# --- CSS connection end ---
 
 scrolled_window_gnomehotk_topic = Gtk.ScrolledWindow()
 scrolled_window_gnomehotk_topic.set_policy(Gtk.PolicyType.AUTOMATIC, Gtk.PolicyType.AUTOMATIC)
@@ -22,11 +37,15 @@ back_button.set_halign(Gtk.Align.START)
 back_button.set_hexpand(False)
 main_box_panel_gnomehotk_topic.append(back_button)
 
-# Label
+
 label = Gtk.Label(label=hotkeys_translations[current_language]["gnomehotk"])
-dynamic_labels.append((label, "hotkeys"))
-label.add_css_class("intro-label")
+dynamic_labels.append((label, "gnomehotk"))
+label.add_css_class("gnome_hotk_text")
+label.set_wrap(True)
+label.set_use_markup(True)
+label.set_xalign(0)
 main_box_panel_gnomehotk_topic.append(label)
+print("[DEBUG:after label] label text:", label.get_text())
 
 scrolled_window_gnomehotk_topic.set_child(main_box_panel_gnomehotk_topic)
 
--- translations/hotkeys_chapters.py
+++ translations/hotkeys_chapters.py
@@ -1,13 +1,46 @@
 hotkeys_translations = {
     "en": {
         "introduction_topic": "This section introduces you to hotkeys in Linux environments...",
-        "gnomehotk": "Hi test1",
+        "gnomehotk": """hi test 2""",
         "kdehotk": "KDE hotkeys are highly customizable. For example: Meta+Space opens KRunner...",
         "terminalhotk": "Terminal hotkeys speed up your workflow. For example: Ctrl+Shift+T opens a new tab...",
     },
     "ru": {
         "introduction_topic": "В этом разделе вы познакомитесь с горячими клавишами в Linux...",
-        "gnomehotk": "Привет тест1",
+        "gnomehotk": """<b>Super</b>: Открыть обзор (Activities Overview) для поиска приложений, окон или рабочих столов.
+- <span color="#00B7EB"><b>Alt + Tab</b></span>: Переключение между открытыми приложениями.
+- <span color="#00B7EB"><b>Alt + `</b></span>: Переключение между окнами одного приложения.
+- <span color="#00B7EB"><b>Super + Tab</b></span>: Переключение между приложениями с визуальным эффектом.
+- <span color="#00B7EB"><b>Alt + F2</b></span>: Открыть строку для быстрого ввода команды.
+- <span color="#00B7EB"><b>Super + L</b></span>: Заблокировать экран.
+- <span color="#00B7EB"><b>Ctrl + Alt + Delete</b></span>: Открыть меню выхода (выход, перезагрузка, выключение).
+- <span color="#00B7EB"><b>Print Screen</b></span>: Сделать скриншот (сохранение в «Изображения» или буфер обмена).
+- <span color="#00B7EB"><b>Super + Print Screen</b></span>: Открыть инструмент для выбора области скриншота.
+
+<span size="large" weight="bold"> Управление окнами </span>
+
+- <span color="#00B7EB"><b>Super + стрелка влево/вправо</b></span>: Прикрепить окно к левой/правой половине экрана.
+- <span color="#00B7EB"><b>Super + стрелка вверх</b></span>: Развернуть окно на весь экран.
+- <span color="#00B7EB"><b>Super + стрелка вниз</b></span>: Свернуть/восстановить окно.
+- <span color="#00B7EB"><b>Alt + F4</b></span>: Закрыть активное окно.
+- <span color="#00B7EB"><b>Alt + Space</b></span>: Открыть меню окна (свернуть, закрыть и т.д.).
+- <span color="#00B7EB"><b>Super + M</b></span>: Показать/скрыть уведомления и календарь.
+
+<span size="large" weight="bold"> Рабочие столы </span>
+
+- <span color="#00B7EB"><b>Super + Page Up/Page Down</b></span>: Переключение между рабочими столами.
+- <span color="#00B7EB"><b>Ctrl + Alt + стрелки влево/вправо</b></span>: Переключение между рабочими столами (в некоторых дистрибутивах).
+- <span color="#00B7EB"><b>Super + Shift + Page Up/Page Down</b></span>: Переместить окно на другой рабочий стол.
+- <span color="#00B7EB"><b>Super + T</b></span>: Открыть новый терминал (в Ubuntu).
+
+<span size="large" weight="bold"> Поиск и приложения </span>
+
+- <span color="#00B7EB"><b>Super + A</b></span>: Показать список приложений в обзоре.
+- <span color="#00B7EB"><b>Super + S</b></span>: Открыть поиск (вводите запрос для поиска файлов, приложений, настроек).
+- <span color="#00B7EB"><b>Super + цифра (1–9)</b></span>: Запустить или переключиться на приложение, закрепленное в панели (по порядку).
+""",
+        
+        
         "kdehotk": "Горячие клавиши KDE легко настраиваются. Например: Meta+Space открывает KRunner...",
         "terminalhotk": "Горячие клавиши терминала ускоряют работу. Например: Ctrl+Shift+T открывает новую вкладку...",
     },
